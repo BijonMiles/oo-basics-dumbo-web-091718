532bb1c295dce642f85153a6355ab6ac08cb9d8e
Bijon Miles <bijonmiles01@gmail.com>
2018-09-21 14:44:11 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book
+
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
+
+  # attr_accessor :
+
+  def initialize(title)
+    @title = title
+    # @author_name = author_name
+    @author = author
+  end
+
+  # def page_count=(page_count)
+  #   @page_count = page_count
+  # end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 
+end
+
+book = Book.new("Agatha Christie")
+book.page_count
+book.genre
+book.turn_page
